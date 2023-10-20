96f6058692bd925a0eee15c7053b1a5ea5c5883d
Richard <richard.hape@devacademy.co.nz>
2023-10-21 12:54:08 +1300

    Changed h1 tag in App.tsx file

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <header className="header">
-        <h1>My Collection</h1>
+        <h1>My Comics</h1>
       </header>
       <section className="main">{/* add your code here */}</section>
     </>
