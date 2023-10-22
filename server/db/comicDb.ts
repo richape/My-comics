324a78df54cc23121fa12b27ef485410b3678885
Richard <richard.hape@devacademy.co.nz>
2023-10-22 18:35:32 +1300

    add comics routes

---
+++ server/db/comicDb.ts
@@ -0,0 +1,36 @@
+import connection from './connection.ts'
+import { Comic, ComicData } from '../../models/comics.ts'
+
+const db = connection
+
+//Get all comics
+export async function getAllComics(): Promise<Comic[]> {
+  return db('comics').select()
+}
+
+// Select comics by ID
+export async function getComicById(id: number): Promise<Comic[]> {
+  return db('comics').where({ id }).select()
+}
+
+// Select comics by type
+export async function getComicByType(type: string): Promise<Comic[]> {
+  return db('comics').where({ type }).select()
+}
+
+// Add new comic
+export async function addComic(comic: ComicData): Promise<Comic[]> {
+  return connection('comics')
+    .insert({ ...comic })
+    .returning([
+      'id',
+      'title',
+      'name',
+      'issue',
+      'datePublished',
+      'publisher',
+      'credits',
+      'coverArt',
+      'coverArtist',
+    ])
+}
---
+++ server/routes/comics.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express'
+import * as db from '../db/comicDb.ts'
+
+const router = Router()
+
+router.get('/', async (req, res) => {
+  try {
+    const comics = await db.getAllComics()
+    res.json({ comics })
+  } catch (error) {
+    res.status(500).json({ message: 'error' })
+  }
+})
+
+router.get('/:comicId', async (req, res) => {
+  try {
+    const comicId = Number(req.params.comicId)
+    if (isNaN(comicId)) {
+      res.sendStatus(400)
+      return
+    }
+    const comic = await db.getComicById(comicId)
+    res.json({ comic })
+  } catch (error) {
+    res.sendStatus(500)
+  }
+})
+
+export default router
