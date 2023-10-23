ca81181a3faea995d0c63abe63dc4acb0374ce6a
Richard <richard.hape@devacademy.co.nz>
2023-10-23 15:44:05 +1300

    add img tag to ComicDetails.tsx

--- client/components/ComicDetails.tsx
+++ client/components/ComicDetails.tsx
@@ -11,18 +11,17 @@ export function ComicDetails(props: Props) {
   return (
     <>
       <div className="comic-box">
+        <img src={comic.coverArt} alt={comic.title} />
         <h2>{comic.title}</h2>
         <h3>{comic.name}</h3>
         <h4>{comic.issue}</h4>
         <h4>{comic.datePublished}</h4>
         <h3>{comic.publisher}</h3>
         <h3>{comic.credits}</h3>
+        <h3>{comic.coverArtist}</h3>
       </div>
     </>
   )
 }
 
 export default ComicDetails
-
-// coverArt: string
-// coverArtist: string
