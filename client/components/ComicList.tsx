7d696070befbb5c18b368ac514dc048b20cd9a34
Richard <richard.hape@devacademy.co.nz>
2023-10-24 10:16:43 +1300

    adding some styling

--- client/components/ComicList.tsx
+++ client/components/ComicList.tsx
@@ -7,17 +7,13 @@ function ComicList() {
   return (
     <>
       <div className="section">
-        <div>
-          {isLoading ? (
-            <h3>Hmmmm Where are all my Comics?</h3>
-          ) : error ? (
-            <h3>Can not find my Comics?</h3>
-          ) : data ? (
-            data.map((comic, index) => (
-              <ComicDetails key={index} comic={comic} />
-            ))
-          ) : null}
-        </div>
+        {isLoading ? (
+          <h3>Hmmmm Where are all my Comics?</h3>
+        ) : error ? (
+          <h3>Can not find my Comics?</h3>
+        ) : data ? (
+          data.map((comic, index) => <ComicDetails key={index} comic={comic} />)
+        ) : null}
       </div>
     </>
   )
