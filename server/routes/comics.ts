8aef0d82249cf0a822e1c9336f776a9d946375d1
Richard <richard.hape@devacademy.co.nz>
2023-10-30 10:55:31 +1300

    installed multer to handle file uploads for coverArt

--- server/routes/comics.ts
+++ server/routes/comics.ts
@@ -14,6 +14,17 @@ router.get('/', async (req, res) => {
   }
 })
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, '../../public/images')
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname)
+  },
+})
+
+const upload = multer({ storage })
+
 export default router
 
 // router.get('/:comicId', async (req, res) => {
