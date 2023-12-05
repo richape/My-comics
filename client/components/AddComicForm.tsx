f41955db46a03f84471e46270dec12ae8b98147c
Richard <richard.hape@devacademy.co.nz>
2023-12-06 11:48:23 +1300

    rewriting my AddComicForm

--- client/components/AddComicForm.tsx
+++ client/components/AddComicForm.tsx
@@ -2,49 +2,45 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { addComic } from '../apis/comics'
 import Nav from './Nav'
 import { ComicData } from '../../models/comics'
-import { useRef, useState } from 'react'
+import { useRef } from 'react'
 
 const AddComicForm: React.FC = () => {
   const queryClient = useQueryClient()
   const mutation = useMutation((comicData: ComicData) => addComic(comicData), {
     onSuccess: () => {
+      console.log('Form reset successfully')
+
       queryClient.invalidateQueries(['addComic'])
       formRef.current?.reset()
     },
   })
 
-  const [comicData, setComicData] = useState<ComicData>({
-    title: '',
-    name: '',
-    issue: '',
-    datePublished: '',
-    publisher: '',
-    credits: '',
-    coverArt: null,
-    coverArtist: '',
-  })
-
   const formRef = useRef<HTMLFormElement>(null)
+
+  let newComicData: ComicData | undefined
 
   const handleFormSubmit = (event: React.FormEvent) => {
     event.preventDefault()
     const form = new FormData(event.currentTarget as HTMLFormElement)
 
-    const coverArtInput = form.get('coverArt') as File
-    if (coverArtInput) {
-      setComicData({
-        title: form.get('title')?.toString() || '',
-        name: form.get('name')?.toString() || '',
-        issue: form.get('issue')?.toString() || '',
-        datePublished: form.get('datePublished')?.toString() || '',
-        publisher: form.get('publisher')?.toString() || '',
-        credits: form.get('credits')?.toString() || '',
-        coverArt: coverArtInput,
-        coverArtist: form.get('coverArtist')?.toString() || '',
-      })
-    }
+    const coverArtInput = form.get('coverArt') as File | null
 
-    mutation.mutate(comicData)
+    newComicData = {
+      title: form.get('title')?.toString() || '',
+      name: form.get('name')?.toString() || '',
+      issue: form.get('issue')?.toString() || '',
+      datePublished: form.get('datePublished')?.toString() || '',
+      publisher: form.get('publisher')?.toString() || '',
+      credits: form.get('credits')?.toString() || '',
+      coverArt: coverArtInput || null,
+      coverArtist: form.get('coverArtist')?.toString() || '',
+    }
+  }
+
+  console.log(newComicData)
+
+  if (newComicData) {
+    mutation.mutate(newComicData)
   }
 
   return (
@@ -81,22 +77,22 @@ const AddComicForm: React.FC = () => {
             <label htmlFor="credits">Credits:</label>
             <input type="text" id="credits" name="credits" />
           </div>
-          <div className="form-group">
+          {/* <div className="form-group">
             <label htmlFor="coverArt">Cover Art:</label>
             <input type="file" id="coverArt" name="coverArt" accept="image/" />
-          </div>
+          </div> */}
           <div className="form-group">
             <label htmlFor="coverArtist">Cover Artist:</label>
             <input type="text" id="coverArtist" name="coverArtist" />
           </div>
-          {comicData.coverArt ? (
+          {/* {comicData.coverArt ? (
             <div className="form-group">
               <img
                 src={URL.createObjectURL(comicData.coverArt)}
                 alt="Cover Art"
               />
             </div>
-          ) : null}
+          ) : null} */}
           <button type="submit" className="add-button">
             Add Comic
           </button>
--- models/comics.ts
+++ models/comics.ts
@@ -17,6 +17,6 @@ export interface ComicData {
   datePublished: string
   publisher: string
   credits: string
-  coverArt: File | null
+  coverArt?: File | null
   coverArtist: string
 }
